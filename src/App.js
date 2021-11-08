c7bfe7b7e837bbf9d0efc5afc54a95304244e630
amDeq <amyadav@deqode.com>
2021-11-08 19:04:11 +0530

    Add: React Composition

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./Components/ExpenseItem";
+import Expenses from "./Components/Expenses/Expenses";
 function App() {
   const expenses = [
     {
@@ -24,23 +24,27 @@ function App() {
   return (
     <div>
       <h2>Let's get started!!</h2>
-      <p>This is visiable</p>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
+      <Expenses expenses={expenses} />
     </div>
+    // <div>
+    //   <h2>Let's get started!!</h2>
+    //   <p>This is visiable</p>
+    //   <ExpenseItem
+    //     title={expenses[0].title}
+    //     amount={expenses[0].amount}
+    //     date={expenses[0].date}
+    //   />
+    //   <ExpenseItem
+    //     title={expenses[1].title}
+    //     amount={expenses[1].amount}
+    //     date={expenses[1].date}
+    //   />
+    //   <ExpenseItem
+    //     title={expenses[2].title}
+    //     amount={expenses[2].amount}
+    //     date={expenses[2].date}
+    //   />
+    // </div>
   );
 }
 
--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,25 +1,29 @@
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
+import Card from "../UI/Card";
 function ExpenseItem(props) {
   // const expenseDate = new Date(2021, 2, 28);
   // const expenseTitle = "Car insurance";
   // const expenseAmount = 267.9;
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  // const month = props.date.toLocaleString("en-US", { month: "long" });
+  // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  // const year = props.date.getFullYear();
   return (
     <>
-      <div className="expense-item">
+      <Card className="expense-item">
         {/* <div>{props.date.toISOString()}</div> */}
-        <div>
+        {/* <div>
           <div>{month}</div>
           <div>{year}</div>
           <div>{day}</div>
-        </div>
+        </div> */}
+        <ExpenseDate date={props.date} />
         <div className="expense-item__description ">
           <h2>{props.title}</h2>
           <div className="expense-item__price">{props.amount}</div>
         </div>
-      </div>
+      </Card>
     </>
   );
 }
