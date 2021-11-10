ce52da9d3262877253ea399d5d0286f8126c332f
amDeq <amyadav@deqode.com>
2021-11-11 00:00:42 +0530

    working on diff state

--- src/Components/NewExpenses/ExpenseForm.js
+++ src/Components/NewExpenses/ExpenseForm.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmmount, setenteredAmmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    // console.log(event.target.value);
+    setEnteredTitle(event.target.value);
+  };
+  const ammountChangeHandler = (event) => {
+    setenteredAmmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
   };
   return (
     <>
@@ -14,11 +24,21 @@ const ExpenseForm = () => {
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" />
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={ammountChangeHandler}
+            />
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2022-12-31" />
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              onChange={dateChangeHandler}
+            />
           </div>
           <div className="new-expense__actions">
             <button type="submit">Add Expense</button>
